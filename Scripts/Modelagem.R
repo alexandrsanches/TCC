85ba28696ea2b763d2bf9d85e21429bb6a4d9d5f
Alexandre Sanches <al.sanches@icloud.com>
2021-10-21 12:30:54 -0300

    Inclusão da conclusão

--- Scripts/Modelagem.R
+++ Scripts/Modelagem.R
@@ -10,7 +10,6 @@ switch (Sys.info()["sysname"],
 library(rio)
 library(plm)
 library(lmtest)
-require(tseries)
 library(stargazer)
 
 # Obtenção e formatação dos dados ----
@@ -39,6 +38,13 @@ summary(reg.agrupada.cambio)
 reg.agrupada.ipca <- plm(expect_ipca ~ surpresa, data = base, model = "pooling")
 summary(reg.agrupada.ipca)
 
+stargazer(reg.agrupada.cambio, reg.agrupada.ipca, reg.agrupada.selic,
+          align = TRUE,
+          title = "Resultado das regressões com MQO agrupado",
+          dep.var.labels = c("Expectativa do câmbio", "Expectativa do IPCA", "Expectativa da Selic"),
+          covariate.labels = c("Surpresa", "Constante"),
+          notes = "Fonte: elaboração própria utilizando R 4.1.1")
+
 ## Efeitos fixos ----
 
 ### Expectativas da Selic ----
@@ -58,6 +64,13 @@ summary(fixef(reg.ef.cambio))
 reg.ef.ipca <- plm(expect_ipca ~ surpresa, data = base, index = "data", model = "within")
 summary(reg.ef.ipca)
 summary(fixef(reg.ef.ipca))
+
+stargazer(reg.ef.cambio, reg.ef.ipca, reg.ef.selic,
+          align = TRUE,
+          title = "Resultado das regressões com Modelo de Efeito Fixo",
+          dep.var.labels = c("Expectativa do câmbio", "Expectativa do IPCA", "Expectativa da Selic"),
+          covariate.labels = c("Surpresa", "Constante"),
+          notes = "Fonte: elaboração própria utilizando R 4.1.1 com o pacote plm")
 
 ## Efeito aleatório ----
 
@@ -110,19 +123,19 @@ phtest(reg.ef.ipca, reg.ea.ipca)
 ## Dependência transversal ----
 
 pcdtest(reg.ea.selic, test = "cd")
-pcdtest(reg.ea.cambio, test = "cd")
-pcdtest(reg.ea.ipca, test = "cd")
+pcdtest(reg.agrupada.cambio, test = "cd")
+pcdtest(reg.ef.ipca, test = "cd")
 
 ## Normalidade dos resíduos ----
 
 shapiro.test(reg.ea.selic$residuals[0:5000])
-shapiro.test(reg.ea.cambio$residuals[0:5000])
-shapiro.test(reg.ea.ipca$residuals[0:5000])
+shapiro.test(reg.agrupada.cambio$residuals[0:5000])
+shapiro.test(reg.ef.ipca$residuals[0:5000])
 
 ## Homocedasticidade dos resíduos ----
 
-bptest(reg.ea.cambio)
-bptest(reg.ea.ipca)
+bptest(reg.agrupada.cambio)
+bptest(reg.ef.ipca)
 bptest(reg.ea.selic)
 
 ## Correlação serial ----
@@ -142,3 +155,65 @@ pwtest(reg.pooled, effect = "time")
 ## Raiz unitária ----
 adf.test(base$surpresa, k = 2)
 
+# TESTES ----
+### Expectativas do câmbio ----
+
+
+
+reg.agrupada.cambio <- plm(expect_cambio ~ surpresa, data = base, model = "pooling")
+summary(reg.agrupada.cambio)
+
+### Expectativas do IPCA ----
+
+reg.ef.ipca <- plm(expect_ipca ~ surpresa, data = base, index = "data", model = "within")
+summary(reg.ef.ipca)
+print(xtable(summary(fixef(reg.ef.ipca))), type = "latex")
+
+### Expectativas da Selic ----
+
+reg.ea.selic <- plm(expect_selic ~ surpresa, data = base, index = "data", model = "random",
+                    random.method = "walhus")
+summary(reg.ea.selic)
+
+stargazer(reg.agrupada.cambio, reg.ef.ipca, reg.ea.selic,
+          align = TRUE,
+          title = "Resultado das regressões",
+          dep.var.labels = c("Expectativa do câmbio", "Expectativa do IPCA", "Expectativa da Selic"),
+          covariate.labels = c("Surpresa", "Constante"),
+          notes = "Fonte: elaboração própria utilizando R 4.1.1")
+
+nobs(reg.agrupada.cambio)
+nobs(reg.ef.ipca)
+nobs(reg.ea.selic)
+
+plot(ranef(reg.ea.selic),
+     xlab = "Instituição",
+     ylab = "Coeficiente")
+
+ea <- data.frame(Index = 0:95,
+                 Instituicao = NA,
+                 Coeficiente = ranef(reg.ea.selic))
+
+ea$Instituicao <- as.numeric(rownames(ea))
+
+xyplot(Coeficiente ~ Index,
+       data = ea,
+       pch = 21, 
+       type = c("p", "g", "smooth"),
+       xlab = "",
+       ylab = "Coeficiente")
+
+cloud(Coeficiente ~ Index * Instituicao, 
+      data = ea)
+
+ggplot(ea, aes(Index, Coeficiente, label = Instituicao)) +
+  geom_point() +
+  geom_text(check_overlap = TRUE,
+            vjust = -0.7)
+
+## Exportar tabelas para LatEx ----
+teste <- data.frame(Variável = c("Taxa básica de juros", "Taxa nominal de câmbio", "Taxa de câmbio nominal"),
+                    Abreviação = c("$selic$", "$cambio$", "$ipca$"),
+                    Período = c("janeiro de 2003 a dezembro de 2020", "janeiro de 2003 a dezembro de 2020", "janeiro de 2003 a dezembro de 2020"))
+
+print(xtable(teste), type = "latex", include.rownames = FALSE)
--- Textos/5 - Conclusão.md
+++ Textos/5 - Conclusão.md
@@ -1,2 +1,6 @@
 # Conclusão
 
+Neste artigo, buscou-se identificar como cada instituição reage frente à um choque de política monetária. Para identificar os choques não antecipados de política monetária, foi construída uma metodologia original usando os microdados do Boletim Focus. A medida de surpresa representa a diferença entre a variação da meta Selic, definida pelo Copom, e a variação esperada pela instituição, portanto, trata-se do erro de previsão de uma dada instituição.
+
+Foi constatado que, durante o período analisado, o mercado foi surpreendido 
+
